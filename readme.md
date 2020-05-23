794193342b478b6e776ef04fa850e46544f187ed
Juan Gonzalez <jgu1@hotmail.com>
2020-05-23 11:18:22 -0400

    Change methodology from Vue to Vanilla, and almost finished.

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Our version of the Pomodoro Clock
+## Made with (not)love by JP and CM
+
+- [x] Start breakTimer when workTimer reaches 0
+- [ ] Make stop and pause functions
+- [x] Increase and decrease functionality for each timer
--- script.js
+++ script.js
@@ -1,56 +1,106 @@
-new Vue({
-	el: "#app",
-	data: {
-		title: "Pomodoro Clock",
-		time: "00:00",
-		timer: 25,
-		seconds: 0,
-		minutes: 0,
-		shutDown: 0,
-        timerRunning: false,
-        breakTimer: 10,
-        shutDownBreak: 0,
-        breakRunning: false
-	},
-	methods: {
-		startTimer() {
-			this.minutes = this.timer;
-			this.timerRunning = true;
-			this.shutDown = setInterval(() => {
-				if (this.seconds === 0) {
-                    this.seconds = 60;
-                    this.minutes--;
-				}
-                this.seconds--;
-                this.checkTimer(this.minutes, this.seconds);
-			}, 1000);
-		},
-		stopTimer() {
-			this.timerRunning = false;
-            this.breakRunning = false;
-			clearInterval(this.shutDown);
-            this.seconds = 0;
-            this.minutes = this.timer;
-		},
-		pauseTimer() {
-            this.timerRunning = false;
-            this.breakRunning = false;
-			clearInterval(this.shutDown);
-		},
-		increaseTimer() {
-			this.timer++;
-		},
-		decreaseTimer() {
-			if (this.timer > 0) {
-				this.timer--;
+class Timer {
+	constructor(minutes) {
+		this.minutes = minutes;
+		this.seconds = 0;
+		this.shutdown = null;
+		this.timerRunning = false;
+		this.workTimer = true;
+	}
+
+	startTimer() {
+		this.timerRunning = true;
+		this.shutdown = setInterval(() => {
+			if (this.seconds === 0) {
+				this.seconds = 60;
+				this.minutes--;
 			}
-        },
-        checkTimer(minutes, seconds) {
-            if (minutes === 0 && seconds === 0) {
-                // do something
-                alert('erei marico?')
-                return
-            }
-        }
-	},
+			this.seconds--;
+			this.updateTimerDisplay();
+
+			if (this.minutes === 0 && this.seconds === 0) {
+				this.switchTimer();
+			}
+		}, 300);
+	}
+
+	switchTimer() {
+		this.reset();
+		if (this.workTimer) {
+			breakTimer.startTimer();
+		} else {
+			workTimer.startTimer();
+		}
+		this.workTimer = !this.workTimer;
+	}
+
+	reset() {
+		clearInterval(this.shutdown);
+	}
+
+	updateTimerDisplay() {
+		const minutes = this.minutes.toString().padStart(2, "0");
+		const seconds = this.seconds.toString().padStart(2, "0");
+		display.textContent = `${minutes}:${seconds}`;
+	}
+	increase(target) {
+		if(target.parentElement.classList.contains('work-time')) {
+			this.minutes = parseInt(workTimeDisplay.textContent);
+			workTimeDisplay.textContent = ++this.minutes;
+		} else {
+			this.minutes = parseInt(breakTimeDisplay.textContent);
+			breakTimeDisplay.textContent = ++this.minutes;
+		}
+	}
+
+	decrease(target) {
+		if(target.parentElement.classList.contains('work-time')) {
+			this.minutes = parseInt(workTimeDisplay.textContent);
+			if (this.minutes > 0) {
+				workTimeDisplay.textContent = --this.minutes;
+			}
+		} else {
+			this.minutes = parseInt(breakTimeDisplay.textContent);
+			if(this.minutes > 0) {
+				breakTimeDisplay.textContent = --this.minutes;
+			}
+		}
+	}
+}
+
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const stopButton = document.getElementById("stop");
+const increaseButton = document.querySelectorAll(".increase");
+const decreaseButton = document.querySelectorAll(".decrease");
+const display = document.getElementById("timer-display");
+const activeTimerDisplay = document.getElementById("active-timer-display");
+
+/* get displays of the timers */
+const workTimeDisplay = document.getElementById("work-time-display");
+const breakTimeDisplay = document.getElementById("break-time-display");
+
+let workTimer = new Timer(2);
+let breakTimer = new Timer(1);
+
+playButton.addEventListener("click", () => {
+	workTimer.startTimer();
+});
+
+pauseButton.addEventListener("click", () => {
+	workTimer.reset();
+	breakTimer.reset();
+});
+
+stopButton.addEventListener("click", () => {});
+
+increaseButton.forEach((button) => {
+	button.addEventListener("click", event => {
+		workTimer.increase(event.target);
+	});
+});
+
+decreaseButton.forEach((button) => {
+	button.addEventListener("click", event => {
+		workTimer.decrease(event.target);
+	});
 });
